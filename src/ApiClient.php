55e79d98d823b84c7f537a10ead95e5dce068958
vicek <vicek.skora@laposte.net>
2018-02-12 15:18:46 +0100

    test init

---
+++ src/ApiClient.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Project\Filer\client;
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\RequestException;
+use Psr\Http\Message\ResponseInterface;
+
+class ApiClient
+{
+    protected $INSTANCE = null;
+    protected $cli;
+
+    public static function getInstance()
+    {
+        if (is_null(self::$INSTANCE)) {
+            self::$INSTANCE = new ApiClient();
+        }
+        return self::$INSTANCE;
+    }
+
+    protected function __construct()
+    {
+        $cli = new GuzzleClient(['base_uri' => 'https://foo.com/api/']);
+    }
+
+    public function createFile(array $data = [])
+    {
+        $promise = $this->cli->postAsync('file', $data);
+        $promise->then(
+            function(ResponseInterface $res) {
+                echo $res->getStatusCode() . "\n";
+            },
+            function(RequestException $e) {
+                echo $e->getMessage() . "\n";
+                echo $e->getRequest()->getMethod();
+            }
+        );
+    }
+
+    public function getFile(int $id)
+    {
+        $promise = $this->cli->getAsync('file/' . $id);
+        $promise->then(
+            function(ResponseInterface $res) {
+                echo $res->getStatusCode() . "\n";
+            },
+            function(RequestException $e) {
+                echo $e->getMessage() . "\n";
+                echo $e->getRequest()->getMethod();
+            }
+        );
+    }
+
+    public function updateFile(int $id, array $data = [])
+    {
+        $promise = $this->cli->putAsync('file/' . $id, $data);
+        $promise->then(
+            function(ResponseInterface $res) {
+                echo $res->getStatusCode() . "\n";
+            },
+            function(RequestException $e) {
+                echo $e->getMessage() . "\n";
+                echo $e->getRequest()->getMethod();
+            }
+        );
+    }
+
+    public function deleteFile(int $id)
+    {
+        $promise = $this->cli->deleteAsync('file/' . $id);
+        $promise->then(
+            function(ResponseInterface $res) {
+                echo $res->getStatusCode() . "\n";
+            },
+            function(RequestException $e) {
+                echo $e->getMessage() . "\n";
+                echo $e->getRequest()->getMethod();
+            }
+        );
+    }
+}
+
---
+++ vendor/phpro/grumphp/src/Composer/DevelopmentIntegrator.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace GrumPHP\Composer;
+
+use Composer\Script\Event;
+use GrumPHP\Collection\ProcessArgumentsCollection;
+use GrumPHP\Process\ProcessFactory;
+use GrumPHP\Util\Filesystem;
+
+class DevelopmentIntegrator
+{
+    /**
+     * This method makes sure that GrumPHP registers itself during development.
+     */
+    public static function integrate(Event $event)
+    {
+        $filesystem = new Filesystem();
+
+        $composerBinDir = $event->getComposer()->getConfig()->get('bin-dir');
+        $executable = getcwd() . '/bin/grumphp';
+        $composerExecutable =  $composerBinDir . '/grumphp';
+        $filesystem->copy(
+            self::noramlizePath($executable),
+            self::noramlizePath($composerExecutable)
+        );
+
+        $commandlineArgs = ProcessArgumentsCollection::forExecutable($composerExecutable);
+        $commandlineArgs->add('git:init');
+
+        $process = ProcessFactory::fromArguments($commandlineArgs);
+        $process->run();
+        if (!$process->isSuccessful()) {
+            $event->getIO()->write(
+                '<fg=red>GrumPHP can not sniff your commits. Did you specify the correct git-dir?</fg=red>'
+            );
+            $event->getIO()->write('<fg=red>' . $process->getErrorOutput() . '</fg=red>');
+            return;
+        }
+
+        $event->getIO()->write('<fg=yellow>' . $process->getOutput() . '</fg=yellow>');
+    }
+
+    /**
+     * @param $path
+     *
+     * @return string
+     */
+    private static function noramlizePath($path)
+    {
+        return strtr($path, '/', DIRECTORY_SEPARATOR);
+    }
+}
